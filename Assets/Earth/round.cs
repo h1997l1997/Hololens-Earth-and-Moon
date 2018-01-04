4a16149fdd9d52b4ef38e1137b9db4e38b2319b9
BenBoErBa-ZhangShiBo <610079410@qq.com>
2018-01-05 00:21:22 +0800

    none

---
+++ Assets/Earth/round.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class round : MonoBehaviour {
+
+    private GameObject moon;
+    private GameObject earth;
+
+	// Use this for initialization
+	void Start () {
+        earth = GameObject.Find("Earth");
+        moon = GameObject.Find("Moon");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moon.transform.RotateAround(earth.transform.position, Vector3.up, 0.06f);
+        transform.Rotate(0, 20 * Time.deltaTime, 0);
+    }
+}
